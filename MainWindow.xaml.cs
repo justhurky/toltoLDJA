99f222ce5d38f0c6c3f738d3e1dfa88d38bb6efe
Hurky <lokid700@gmail.com>
2023-05-27 12:26:41 +0200

    class letrehozva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace tolto
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -72,5 +69,6 @@ namespace tolto
             string mess = "Fő tér.\nÁr: 550 Ft/h\nHelyek száma: 2";
             MessageBox.Show(mess);
         }
+
     }
 }
---
+++ fiok.cs
@@ -0,0 +1,22 @@
+﻿namespace tolto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    class fiok {
+        string felhnev;
+        string pass;
+        string mail;
+
+        public fiok(string felhnev, string pass, string mail)
+        {
+            this.felhnev = felhnev;
+            this.pass = pass;
+            this.mail = mail;
+        }
+
+        public string Felhnev { get => felhnev; set => felhnev = value; }
+        public string Pass { get => pass; set => pass = value; }
+        public string Mail { get => mail; set => mail = value; }
+    }
+}

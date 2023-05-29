b6d3cd1d6bf3cd19763ccaa89772b161c6708560
Hurky <lokid700@gmail.com>
2023-05-29 23:45:54 +0200

    regisztráció

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace tolto
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    
+
     public partial class Window1 : Window
     {
 
@@ -27,37 +28,67 @@ namespace tolto
             InitializeComponent();
             if (!File.Exists("adat.txt"))
             {
-               File.Create("adat.txt");
+                File.Create("adat.txt");
             }
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
 
+
+            string felhasznalonev = textBox.Text;
+            string jelszo = passwordBox.Password;
+            string jelszoIsmetles = passwordBox1.Password;
+            string email = textBox1.Text;
+
             
-            if (string.IsNullOrEmpty(textBox.Text) || string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(passwordBox.Password) || string.IsNullOrEmpty(passwordBox1.Password))
+            if (jelszo != jelszoIsmetles)
             {
-                MessageBox.Show("A felhasználó név, email vagy jelszó üres!");
+                MessageBox.Show("A jelszavak nem egyeznek!");
                 return;
             }
-            if (IsUsernameTaken(textBox.Text) || IsEmailTaken(textBox1.Text))
+
+            
+            string titkosJelszo = EncryptString(jelszo);
+
+            
+            string adatok = $"Felhasználónév: {felhasznalonev}\n" +
+                            $"Titkosított jelszó: {titkosJelszo}\n" +
+                            $"E-mail: {email}\n\n";
+
+            string eleresiUtvonal = "adatok.txt";
+            File.AppendAllText(eleresiUtvonal, adatok);
+
+            MessageBox.Show("Sikeres regisztráció!");
+
+
+        }
+        private string EncryptString(string text)
+        {
+            using (Aes aes = Aes.Create())
             {
-                MessageBox.Show("A felhasználónév vagy email foglalt!");
-                return;
+                byte[] key = Encoding.UTF8.GetBytes("EzAzAesKulcs12345");
+                byte[] iv = Encoding.UTF8.GetBytes("EzAzAesIV123456");
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(cryptoStream))
+                        {
+                            writer.Write(text);
+                        }
+
+                        byte[] encryptedBytes = memoryStream.ToArray();
+                        return Convert.ToBase64String(encryptedBytes);
+                    }
+                }
             }
-            byte[] data = System.Text.Encoding.ASCII.GetBytes(inputString);
-            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
-            String hash = System.Text.Encoding.ASCII.GetString(data);
-        }
-
-        private bool IsEmailTaken(string text)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool IsUsernameTaken(string text)
-        {
-            throw new NotImplementedException();
         }
     }
 }
